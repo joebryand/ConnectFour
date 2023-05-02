77a34d09718f91fff99623dc5617a9574522c72d
joebryand <pimjansen1212@gmail.com>
2023-05-02 16:00:34 +0200

    Wrote class for buttons + implemented UI in game + implemented end screen

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ points = {"red" : 0, "yellow" : 0}
 def main():
     run = True
     clock = pygame.time.Clock()
+    print(game.game_board.get_amound_of_possible_connect_fours())
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
@@ -25,40 +26,66 @@ def main():
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 abs_pos = pygame.mouse.get_pos()
-                col = int((abs_pos[0]-game.window.board_rect[0]) // 70)
-                if abs_pos[0] <= 100:
-                    if game.make_move(find_best_move(game.game_board,game.turn)):
-                        if game.win_check():
-                            game.winner = game.turn
-                        game.change_turn()
-                        game.possible_moves = game.get_possible_moves()
+                if game.gamestate == "dynamic":
 
-                elif abs_pos[0] >= 590:
-                    if game.game_board.cancel_move():
-                        game.change_turn()
-                    game.possible_moves = game.get_possible_moves()
+                    if abs_pos[0] <= 90:
+                        if game.make_move(find_best_move(game.game_board,game.turn)):
+                            if game.win_check():
+                                game.winner = game.turn
+                            game.change_turn()
+                            game.possible_moves = game.get_possible_moves()
 
-                if -1 < col < 7:
-                    if game.make_move(col):
-                        if game.win_check():
-                            game.winner = game.turn
-                        game.change_turn()
-                        game.possible_moves = game.get_possible_moves()
+                    elif abs_pos[0] >= 590:
+                        for button in game.window.in_game_buttons: 
+                            action = button.check_if_clicked(abs_pos)
+                            if action != 0:
+                                break
+                        #print(action)
 
-        WIN.fill((0,0,10))
+                        if action == "CANCEL MOVE":
+                            if game.game_board.cancel_move():
+                                game.change_turn()
+                            game.possible_moves = game.get_possible_moves()
+
+                        if action == "RESET":
+                            game.reset()
+
+                        if action == "HIGHLIGHT":
+                            game.enable_show_possible_moves = not game.enable_show_possible_moves
+
+                    col = int((abs_pos[0]-game.window.board_rect[0]) // 70)
+                    if -1 < col < 7:
+                        if game.make_move(col):
+                            if game.win_check():
+                                game.winner = game.turn
+                            game.change_turn()
+                            game.possible_moves = game.get_possible_moves()
+
+
+                elif game.gamestate == "static":
+                    for button in game.window.game_end_buttons: 
+                        action = button.check_if_clicked(abs_pos)
+                        if action != 0:
+                            break
+                    print(action)
+
+                    if action == "NEW GAME":
+                        game.reset()
+
+
         winner = game.update()
         if winner == 'yellow':
             points["yellow"] += 1
             print(points)
-            game.reset()
+            game.gamestate = "static"
         elif winner == 'red':
             points["red"] += 1
             print(points)
-            game.reset()
+            game.gamestate = "static"
         
         elif len(game.game_board.moves) == 42:
             print(points)
-            game.reset()
+            game.gamestate = "static"
         
 
     pygame.quit()
